fa4ff046f7b74aeb97ef68db7088e1cdfbb0781d
bloodysnowx <genius@tcct.zaq.ne.jp>
2013-02-11 18:53:15 +0900

    画像にフィルタをかけられるように変更

--- EternalBum/EternalBum/BSViewController.m
+++ EternalBum/EternalBum/BSViewController.m
@@ -12,7 +12,12 @@
 #import <AssetsLibrary/AssetsLibrary.h>
 
 @interface BSViewController ()
-
+{
+    BOOL isFiltered;
+    double filterCoeff;
+    UIImage* upImage;
+    UIImage* downImage;
+}
 @property (nonatomic, assign)CGSize visibleArea;
 
 @property (nonatomic, retain) ALAssetsLibrary* assetsLibrary;
@@ -36,6 +41,8 @@ static const int Y_MARGIN = 60;
 	// Do any additional setup after loading the view, typically from a nib.
     self.assetsLibrary = [[ALAssetsLibrary alloc] init];
     [self setUpGesture];
+    filterCoeff = 1.2;
+    isFiltered = YES;
 }
 
 - (void)addGestureWithTarget:(id)target action:(SEL)selector direction:(UISwipeGestureRecognizerDirection)direction {
@@ -48,9 +55,31 @@ static const int Y_MARGIN = 60;
 - (void) setUpGesture
 {
     // 右へ : 未来
-	[self addGestureWithTarget:self action:@selector(prev) direction:UISwipeGestureRecognizerDirectionRight];
+	[self addGestureWithTarget:self action:@selector(next) direction:UISwipeGestureRecognizerDirectionRight];
 	// 左へ : 過去
-	[self addGestureWithTarget:self action:@selector(next) direction:UISwipeGestureRecognizerDirectionLeft];
+	[self addGestureWithTarget:self action:@selector(prev) direction:UISwipeGestureRecognizerDirectionLeft];
+    // Up
+    [self addGestureWithTarget:self action:@selector(up) direction:UISwipeGestureRecognizerDirectionUp];
+    // Down
+    [self addGestureWithTarget:self action:@selector(down) direction:UISwipeGestureRecognizerDirectionDown];
+}
+
+- (void)up
+{
+    filterCoeff += 0.1;
+    if(filterCoeff > 2.0)
+        isFiltered = NO;
+    else [self resetPictures];
+}
+
+- (void)down
+{
+    if(filterCoeff > 0.8)
+    {
+        filterCoeff -= 0.1;
+        isFiltered = filterCoeff < 2.0;
+        [self resetPictures];
+    }
 }
 
 - (void)prev
@@ -125,7 +154,7 @@ static const int Y_MARGIN = 60;
     }];
 }
 
-- (void)relocatePictureView:(UIImageView*)view HeightOffset:(CGFloat)offset
+- (void)relocatePictureView:(UIImageView*)view Image:(UIImage*)origin HeightOffset:(CGFloat)offset
 {
     view.transform = CGAffineTransformMakeRotation(0.0);
     int x = arc4random() % X_MARGIN;
@@ -134,19 +163,164 @@ static const int Y_MARGIN = 60;
     int height = self.backView.frame.size.height - y - arc4random() % Y_MARGIN;
     y += offset;
     view.frame = CGRectMake(x, y, width, height);
-    ALAsset* asset = self.photos[self.currentNum++ % [self.photos count]];
-    ALAssetRepresentation* representation = [asset defaultRepresentation];
-    view.image = [UIImage imageWithCGImage:[representation fullScreenImage]];
-    // self.pictureView.layer.anchorPoint = CGPointMake(0, 0);
-    int deg = (arc4random() % 60 - 30 + 360) % 360;
+    [self setPicture:view Image:origin];
+    int deg = (arc4random() % 60 - 30 + 360 + 180) % 360;
     NSLog(@"x =  %d, y = %d, width = %d, height = %d, deg = %d", x, y, width, height, deg);
     view.transform = CGAffineTransformMakeRotation(M_PI * deg / 180.0f);
 }
 
+- (UIImage*)maskImage:(UIImage*)origin
+{
+    CGImageRef imageRef = origin.CGImage;
+    size_t width  = CGImageGetWidth(imageRef);
+    size_t height = CGImageGetHeight(imageRef);
+    // ピクセルを構成するRGB各要素が何ビットで構成されている
+    size_t bitsPerComponent = CGImageGetBitsPerComponent(imageRef);
+    // ピクセル全体は何ビットで構成されているか
+    size_t bitsPerPixel = CGImageGetBitsPerPixel(imageRef);
+    // 画像の横1ライン分のデータが、何バイトで構成されているか
+    size_t bytesPerRow = CGImageGetBytesPerRow(imageRef);
+    // 画像の色空間
+    CGColorSpaceRef colorSpace = CGImageGetColorSpace(imageRef);
+    // 画像のBitmap情報
+    CGBitmapInfo bitmapInfo = CGImageGetBitmapInfo(imageRef);
+    // 画像がピクセル間の補完をしているか
+    bool shouldInterpolate = CGImageGetShouldInterpolate(imageRef);
+    // 表示装置によって補正をしているか
+    CGColorRenderingIntent intent = CGImageGetRenderingIntent(imageRef);
+    // 画像のデータプロバイダを取得する
+    CGDataProviderRef dataProvider = CGImageGetDataProvider(imageRef);
+    // データプロバイダから画像のbitmap生データ取得
+    CFDataRef tmpData = CGDataProviderCopyData(dataProvider);
+    CFMutableDataRef data = CFDataCreateMutableCopy(0, 0, tmpData);
+    UInt8* buffer = (UInt8*)CFDataGetBytePtr(data);
+    // 1ピクセルずつ画像を処理
+    NSUInteger x, y;
+    for (y = 0; y < height; y++) {
+        for (x = 0; x < width; x++) {
+            UInt8* tmp;
+            tmp = buffer + y * bytesPerRow + x * 4; // RGBAの4つ値をもっているので、1ピクセルごとに*4してずらす
+            // RGB値を取得
+            UInt8 *alpha;
+            alpha = tmp + 3;
+            *(tmp + 0) = (UInt8)10;
+            // NSLog(@"%o", *tmp);
+            // *alpha = 0.5;
+            
+        }
+    }
+    // 効果を与えたデータ生成
+    CFDataRef effectedData = CFDataCreate(NULL, buffer, CFDataGetLength(data));
+    // 効果を与えたデータプロバイダを生成
+    CGDataProviderRef effectedDataProvider = CGDataProviderCreateWithCFData(effectedData);
+    // 画像を生成
+    CGImageRef effectedCgImage = CGImageCreate(width, height, bitsPerComponent, bitsPerPixel, bytesPerRow,
+                                               colorSpace, bitmapInfo, effectedDataProvider, NULL,
+                                               shouldInterpolate, intent);
+    UIImage* effectedImage = [[UIImage alloc] initWithCGImage:effectedCgImage];
+    // データの解放
+    CGImageRelease(effectedCgImage);
+    CFRelease(effectedDataProvider);
+    CFRelease(effectedData);
+    CFRelease(data);
+    
+    return effectedImage;
+}
+
+- (UIImage*)maskImage2:(UIImage*)origin
+{
+    CGImageRef imageRef = origin.CGImage;
+    int width = CGImageGetWidth(imageRef);
+    int height = CGImageGetHeight(imageRef);
+    unsigned char *data = malloc(width * height * sizeof(unsigned char) * 4);
+    CGContextRef bitmapContext = CGBitmapContextCreate(data, width, height, 8, width * 4,
+                                                       CGColorSpaceCreateDeviceRGB(),
+                                                       kCGImageAlphaPremultipliedLast);
+    
+    CGContextDrawImage(bitmapContext, CGRectMake(0,0,width,height), imageRef);
+    unsigned char *bitmap = CGBitmapContextGetData(bitmapContext);
+    double center_x = width / 2.0;
+    double center_y = height / 2.0;
+    for(int x = 0; x < width; ++x)
+    {
+        NSMutableString* str = [NSMutableString new];
+        for(int y = 0; y < height; ++y)
+        {
+            int i = (x + y * width) * 4;
+            double x_from_center = (x - center_x) / center_x;
+            double y_from_center = (y - center_y) / center_y;
+            unsigned char alpha = MIN(255, 255 * MAX(0, (filterCoeff - x_from_center * x_from_center - y_from_center * y_from_center)));
+        
+            bitmap[i + 3] = alpha;
+        // [str appendFormat:@"%d ", alpha];
+        }
+        // NSLog(str);
+    }
+    /*
+    for(int i = 0; i < width * height * 4;){
+        bitmap[i] = bitmap[i++];
+        bitmap[i] = bitmap[i++];
+        bitmap[i] = bitmap[i++];
+        bitmap[i] = 255 * 0.9;  //alpha 半透明にしちゃう
+        i++;
+    }
+    */
+    
+    CGDataProviderRef dataProviderRef;
+    dataProviderRef = CGDataProviderCreateWithData(NULL,
+                                                   bitmap,
+                                                   width * height * 4,
+                                                   NULL);
+    CGImageRef invertCGImage = CGImageCreate(width,
+                                             height,
+                                             8,//size_t bitsPerComponent,
+                                             32,//size_t bitsPerPixel,
+                                             width * 4,//size_t bytesPerRow,
+                                             CGColorSpaceCreateDeviceRGB(),
+                                             kCGImageAlphaLast,
+                                             dataProviderRef,
+                                             NULL,
+                                             0,
+                                             kCGRenderingIntentDefault);
+    
+    UIGraphicsBeginImageContext(CGSizeMake(width, height));
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextDrawImage(context, CGRectMake(0,0,width,height), invertCGImage);
+    UIImage* result = UIGraphicsGetImageFromCurrentImageContext();
+	UIGraphicsEndImageContext();
+    
+    free(data);
+    CGDataProviderRelease(dataProviderRef);
+    // CGImageRelease(imageRef);
+    return result;
+}
+
+- (UIImage*)getCurrentImage
+{
+    ALAsset* asset = self.photos[self.currentNum++ % [self.photos count]];
+    ALAssetRepresentation* representation = [asset defaultRepresentation];
+    return [UIImage imageWithCGImage:[representation fullScreenImage]];
+}
+
+- (void)resetPictures
+{
+    [self setPicture:self.pictureUpView Image:upImage];
+    [self setPicture:self.pictureDownView Image:downImage];
+}
+
+- (void)setPicture:(UIImageView*)view Image:(UIImage*)origin
+{
+    UIImage* image = origin;
+    if(isFiltered) image = [self maskImage2:image];
+    view.image = image;
+}
+
 - (void)relocatePictureView
 {
-    [self relocatePictureView:self.pictureUpView HeightOffset:-self.backView.frame.size.height/4];
-    [self relocatePictureView:self.pictureDownView HeightOffset:self.backView.frame.size.height/4];
+    upImage = [self getCurrentImage];
+    downImage = [self getCurrentImage];
+    [self relocatePictureView:self.pictureUpView Image:upImage HeightOffset:-self.backView.frame.size.height/4];
+    [self relocatePictureView:self.pictureDownView Image:downImage HeightOffset:self.backView.frame.size.height/4];
 }
 
 - (void)calcVisibleArea
