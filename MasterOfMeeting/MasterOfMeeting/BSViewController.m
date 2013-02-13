77bb122e439dec7badb20ab330a99d9c5a5d7e47
Atsushi Iwasa <a-iwasa@biglobe.co.jp>
2013-02-13 18:44:52 +0900

    音声再生を連鎖可能に設定 ファイル名を変更

--- MasterOfMeeting/MasterOfMeeting/BSViewController.m
+++ MasterOfMeeting/MasterOfMeeting/BSViewController.m
@@ -19,21 +19,24 @@
 
 @implementation BSViewController
 
-static const char* const TIME_WARNING_MP3 = "20_ED";
-static const char* const TIME_ENDED_MP3 = "20_ED";
+static const char* const TIME_WARNING_MP3 = "800_WARNING";
+static const char* const TIME_ENDED_MP3 = "810_ENDING";
+static const char* const NOTHING_MP3 = "600_NOTHING";
+static const char* const SEYASEYA_MP3 = "610_SEYASEYA";
 static const int WARNING_TIME = 30;
 static const int ENDING_TIME = 30;
 
-- (void)playChar:(const char* const)path
+- (void)playChar:(const char* const)path WillDelegate:(BOOL)flag
 {
     NSURL* fileURL = [self getURLfromBundleChar:path];
-    [self play:fileURL];
+    [self play:fileURL WillDelegate:flag];
 }
 
-- (void)play:(NSURL*)fileURL
+- (void)play:(NSURL*)fileURL WillDelegate:(BOOL)flag
 {
     [NSObject cancelPreviousPerformRequestsWithTarget:self];
     player = [[AVAudioPlayer alloc] initWithContentsOfURL:fileURL error:nil];
+    player.delegate = flag ? self : nil;
     [player prepareToPlay];
     [player play];
 }
@@ -61,7 +64,7 @@ static const int ENDING_TIME = 30;
     [super viewDidLoad];
     nowCount = 0;
 	// Do any additional setup after loading the view, typically from a nib.
-    NSArray* filePaths = [[NSArray alloc] initWithObjects:@"01_OP", @"20_ED", nil];
+    NSArray* filePaths = [[NSArray alloc] initWithObjects:@"010_OP", @"020_ED", nil];
     fileURLs = [[NSMutableArray alloc] initWithCapacity:[filePaths count]];
     for(NSString* path in filePaths)
         [fileURLs addObject:[self getURLfromBundle:path]];
@@ -75,19 +78,19 @@ static const int ENDING_TIME = 30;
 
 -(IBAction)buttonPushed:(id)sender
 {
-    [self play:fileURLs[nowCount++]];
+    [self play:fileURLs[nowCount++] WillDelegate:NO];
     [self performSelector:@selector(timeWarning) withObject:nil afterDelay:WARNING_TIME];
 }
 
 -(void)timeWarning
 {
-    [self playChar:TIME_WARNING_MP3];
+    [self playChar:TIME_WARNING_MP3 WillDelegate:NO];
     [self performSelector:@selector(timeEnded) withObject:nil afterDelay:ENDING_TIME];
 }
 
 -(void)timeEnded
 {
-    [self playChar:TIME_ENDED_MP3];
+    [self playChar:TIME_ENDED_MP3 WillDelegate:NO];
 }
 
 @end
