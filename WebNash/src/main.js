11e7ea802f7465aba0357f4e37d6cebffed0fafd
bloodysnow <genius@tcct.zaq.ne.jp>
2013-03-27 18:56:26 +0900

    HTMLのひな形作成開始

---
+++ WebNash/src/main.js
@@ -0,0 +1,8 @@
+function main()
+{
+    var Structure = document.HandHistory.Structure.value;
+    var Content = document.HandHistory.Content.value;
+    var hh = Content.split(/\r\n|\r|\n/);
+    alert(hh);
+    // window.open("http://bloodysnow.com");
+}
