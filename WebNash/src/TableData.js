2984e6503638a4a0c55eab9971e966e316811b05
bloodysnow <genius@tcct.zaq.ne.jp>
2013-03-27 17:15:57 +0900

    TableDataのUTと実装完

--- WebNash/src/TableData.js
+++ WebNash/src/TableData.js
@@ -2,7 +2,8 @@ var PlayerData = {
     seat: 0,
     position: "",
     playerName: "",
-    chip: 0
+    chip: 0,
+    posted: 0
 };
 
 var TableData = {
@@ -10,7 +11,7 @@ var TableData = {
     BB: 20,
     SB: 10,
     Ante: 0,
-    Structure: "",
+    Structure: "1,1",
     heroPos: -1,
     heroName: "",
     seats: [],
@@ -23,8 +24,25 @@ var TableData = {
     StartingChip: 0,
     heroIndex: -1,
     getHeroIndex: function() { if (this.heroIndex < 0) this.heroIndex = this.calcHeroIndex(); return this.heroIndex; },
-    calcHeroIndex: function() { },
+    calcHeroIndex: function() {
+        var ret = 0;
+        for(var i = 0; i < this.playerNames.length; ++i) if(this.playerNames[i] == this.heroName) ret = i;
+        return ret;
+    },
     getHeroSeat: function() { return this.seats[this.getHeroIndex()]; },
-    nextButton: function() { },
-    getLivePlayerCount: function() { }
+    nextButton: function() {
+        var nowIndex;
+        for(nowIndex = 0; nowIndex < this.MaxSeatNum; ++nowIndex) if(this.seats[nowIndex] == this.buttonPos) break;
+        for(var i = 1; i < this.MaxSeatNum; ++i) {
+            if(this.seats[(i + nowIndex) % this.MaxSeatNum] > 0) {
+                this.buttonPos = this.seats[(i + nowIndex) % this.MaxSeatNum];
+                break;
+            }
+        }
+    },
+    getLivePlayerCount: function() {
+        var count = 0;
+        for(var i = 0; i < this.chips.length; ++i) if(this.chips[i] > 0) ++count;
+        return count;
+    }
 };
--- WebNash/test/TableDataTest.js
+++ WebNash/test/TableDataTest.js
@@ -1,9 +1,91 @@
 TestCase("TableDataTest", {
     setUp: function() {
         this.tableData = Object.create(TableData);
+        this.tableData.heroName = "HERO";
+        this.tableData.playerNames = [ "Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "HERO", "India" ];
+        this.tableData.seats = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+        this.tableData.buttonPos = 1;
+        this.tableData.chips = [ 100, 100, 100, 100, 100, 100, 100, 100, 100 ];
     },
+
     "test create": function() {
         var result = Object.create(TableData);
         assertEquals(9, result.MaxSeatNum);
+        assertEquals("1,1", result.Structure);
+    },
+
+    "test getHeroIndex = 0": function() {
+        this.tableData.playerNames = [ "HERO", "test" ];
+        assertEquals(0, this.tableData.getHeroIndex());
+    },
+    "test getHeroIndex = 1": function() {
+        this.tableData.playerNames = [ "hoge", "HERO", "test" ];
+        assertEquals(1, this.tableData.getHeroIndex());
+    },
+    "test getHeroIndex = 7": function() {
+        assertEquals(7, this.tableData.getHeroIndex());
+    },
+
+    "test calcHeroIndex": function() { },
+
+    "test getHeroSeat = 2": function() {
+        this.tableData.playerNames = [ "hoge", "HERO", "test" ];
+        this.tableData.seats = [ "1", "2", "3" ];
+        assertEquals(2, this.tableData.getHeroSeat());
+    },
+    "test getHeroSeat = 5": function() {
+        this.tableData.playerNames = [ "hoge", "Alpha", "Bravo", "HERO", "test" ];
+        this.tableData.seats = [ "1", "2", "3", "5", "7" ];
+        assertEquals(5, this.tableData.getHeroSeat());
+    },
+    "test getHeroSeat = 8": function() {
+        assertEquals(8, this.tableData.getHeroSeat());
+    },
+
+    "test nextButton normal": function() {
+        assertEquals(1, this.tableData.buttonPos);
+        this.tableData.nextButton();
+        assertEquals(2, this.tableData.buttonPos);
+        this.tableData.nextButton();
+        assertEquals(3, this.tableData.buttonPos);
+        this.tableData.nextButton();
+        assertEquals(4, this.tableData.buttonPos);
+        this.tableData.nextButton();
+        assertEquals(5, this.tableData.buttonPos);
+        this.tableData.nextButton();
+        assertEquals(6, this.tableData.buttonPos);
+        this.tableData.nextButton();
+        assertEquals(7, this.tableData.buttonPos);
+        this.tableData.nextButton();
+        assertEquals(8, this.tableData.buttonPos);
+        this.tableData.nextButton();
+        assertEquals(9, this.tableData.buttonPos);
+        this.tableData.nextButton();
+        assertEquals(1, this.tableData.buttonPos);
+        this.tableData.nextButton();
+    },
+    "test nextButton another": function() {
+        this.tableData.playerNames = [ "hoge", "Alpha", "Bravo", "HERO", "test" ];
+        this.tableData.seats = [ "1", "2", "3", "5", "7" ];
+        assertEquals(1, this.tableData.buttonPos);
+        this.tableData.nextButton();
+        assertEquals(2, this.tableData.buttonPos);
+        this.tableData.nextButton();
+        assertEquals(3, this.tableData.buttonPos);
+        this.tableData.nextButton();
+        assertEquals(5, this.tableData.buttonPos);
+        this.tableData.nextButton();
+        assertEquals(7, this.tableData.buttonPos);
+        this.tableData.nextButton();
+        assertEquals(1, this.tableData.buttonPos);
+        this.tableData.nextButton();
+    },
+
+    "test getLivePlayerCount = 9": function() {
+        assertEquals(9, this.tableData.getLivePlayerCount());
+    },
+    "test getLivePlayerCount = 7": function() {
+        this.tableData.chips = [ 100, 0, 100, 0, 100, 100, 100, 100, 100 ];
+        assertEquals(7, this.tableData.getLivePlayerCount());
     }
 });
