c61be39ad02be0ecf02eff4aebea6fb6cb37694b
bloodysnow <genius@tcct.zaq.ne.jp>
2013-03-27 13:07:45 +0900

    HandRangeの追加

---
+++ HyahhooTracker/HyahhooTracker/BSHandRange.m
@@ -0,0 +1,13 @@
+//
+//  BSHandRange.m
+//  HyahhooTracker
+//
+//  Created by 岩佐 淳史 on 13/03/17.
+//  Copyright (c) 2013年 bloodysnow. All rights reserved.
+//
+
+#import "BSHandRange.h"
+
+@implementation BSHandRange
+
+@end
