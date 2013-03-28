975c5c2978e249a7886db60102903b6a939fdf4b
bloodysnow <genius@tcct.zaq.ne.jp>
2013-03-28 22:21:09 +0900

    途中まで移植

--- WebNash/src/PSHandReader.js
+++ WebNash/src/PSHandReader.js
@@ -1,25 +1,121 @@
 var PSHandReader = {
     getHeroName:function(hh) {
-        var regex = new RegExp("Dealt¥sto¥s(.+)¥s¥[[2-9TJQKA][shdc]¥s[2-9TJQKA][shdc]¥]");
         for(var i = 0; i < hh.length; ++i)
         {
-            if(hh[i].match(regex))
+            if(hh[i].match(/Dealt¥sto¥s(.+)¥s¥[[2-9TJQKA][shdc]¥s[2-9TJQKA][shdc]¥]/))
                 return RegExp.$1;
         }
         return null;
     },
-    getStartingChip:function(heroName, hh, defaultValue) { },
+    getStartingChip:function(heroName, hh, defaultValue) {
+        var ret = defaultValue;
+        for(var i = 0; i < hh.length; ++i)
+        {
+            if(hh[i].match(/Seat¥s([0-9]+):¥s(.+)¥s¥(([0-9]+)¥sin¥schips¥)/) && RegExp.$2 == heroName)
+            {
+                ret = RegExp.$3;
+                break;
+            }
+        }
+        return ret;
+    },
     // getHHLineList:function(hh) { },
     // getNowHH:function(hh, lineList, backNum) { },
-    setBBSB:function(data, line) { },
-    getButtonPos:function(line) { },
-    getStartSituation:function(result, hh, line) { },
-    calcAntePayment:function(result, hh, line) { },
-    calcBlindPayment:function(blind, result, hh, line) { },
-    calcBetPayment:function(result, line) { },
-    calcCallPayment:function(result, line) { },
-    calcRaisePayment:function(result, line) { },
-    calcUncalledPayment:function(result, line) { },
+    setBBSB:function(data, line) {
+        if(line.match(/¥(([0-9]+)¥u002f([0-9]+)¥)/))
+        {
+            data.SB = RegExp.$1;
+            data.BB = RegExp.$2;
+        }
+    },
+    getButtonPos:function(line) {
+        line.match(/Seat¥s#([0-9]+)¥sis¥sthe¥sbutton/);
+        return RegExp.$1;
+    },
+    getStartSituation:function(result, hh, line) {
+        for(var i = 0; i < result.MaxSeatNum; ++i)
+        {
+            if(hh[line + i + 1].match(/Seat¥s([0-9]+):¥s(.+)¥s¥(([0-9]+)¥sin¥schips¥)/))
+            {
+                result.seats[i] = RegExp.$1;
+                result.playerNames[i] = RegExp.$2;
+                result.chips[i] = RegExp.$3;
+                result.posted[i] = 0;
+            }
+            else return i;
+        }
+        return result.MaxSeatNum;
+    },
+    calcAntePayment:function(result, hh, line) {
+        for(var i = 0; i < result.MaxSeatNum; ++i)
+        {
+            if(hh[line + i + 1].match(/(.+):¥sposts¥sthe¥sante¥s([0-9]+)/))
+            {
+                var j = result.getIndexFromName(RegExp.$1);
+                result.Ante = Math.max(result.Ante, RegExp.$2);
+                result.chips[j] -= RegExp.$2;
+                result.pot += parseInt(RegExp.$2);
+            }
+            else return i;
+        }
+        return result.MaxSeatNum;
+    },
+    calcBlindPayment:function(blind, result, hh, line) {
+        var regexp = new RegExp("(.+):¥¥sposts¥¥s" + blind + "¥¥sblind¥¥s([0-9]+)");
+        if(hh[line + 1].match(regexp))
+        {
+            var i = result.getIndexFromName(RegExp.$1);
+            result.posted[i] = parseInt(RegExp.$2);
+            result.chips[i] -= result.posted[i];
+            result.pot += result.posted[i];
+            return 1;
+        }
+        return 0;
+    },
+    calcBlindsPayment:function(result, hh, line) {
+        var ret = this.calcBlindPayment("small", result, hh, line);
+        ret += this.calcBlindPayment("big", result, hh, line + ret);
+        return ret;
+    },
+    calcBetPayment:function(result, line) {
+        if(line.match(/(.+):¥sbets¥s([0-9]+)/)) {
+            var i = result.getIndexFromName(RegExp.$1);
+            result.chips[i] -= RegExp.$2;
+            result.pot += RegExp.$2;
+            result.posted[i] += RegExp.$2;
+            return true;
+        }
+        return false;
+    },
+    calcCallPayment:function(result, line) {
+        if(line.match(/(.+):¥scalls¥s([0-9]+)/)) {
+            var i = result.getIndexFromName(RegExp.$1);
+            result.chips[i] -= RegExp.$2;
+            result.pot += RegExp.$2;
+            result.posted[i] += RegExp.$2;
+            return true;
+        }
+        return false;
+    },
+    calcRaisePayment:function(result, line) {
+        if(line.match(/(.+):¥sraises¥s([0-9]+)¥sto¥s([0-9]+)/)) {
+            var i = result.getIndexFromName(RegExp.$1);
+            result.chips[i] -= RegExp.$3 - result.posted[i];
+            result.pot += RegExp.$3 - result.posted[i];
+            result.posted[i] = parseInt(RegExp.$3);
+            return true;
+        }
+        return false;
+    },
+    calcUncalledPayment:function(result, line) {
+        if(line.match(/Uncalled¥sbet¥s¥(([0-9]+)¥)¥sreturned¥sto¥s(.+)/)) {
+            var i = result.getIndexFromName(RegExp.$2);
+            result.chips[i] += RegExp.$1;
+            result.pot -= RegExp.$1;
+            return true;
+        }
+        return false;
+    },
     calcCollectPayment:function(result, line) { },
     readNow: function(history) { },
     readNext: function(history) { }
--- WebNash/test/PSHandReaderTest.js
+++ WebNash/test/PSHandReaderTest.js
@@ -1,8 +1,128 @@
 TestCase("PSHandReaderTest", {
     setUp: function() {
         this.reader = Object.create(PSHandReader);
+        this.tableData = Object.create(TableData);
+        this.reader.getStartSituation(this.tableData, ["Table '674374357 1' 6-max Seat #1 is the button",
+                                                               "Seat 1: SLIMEE_11 (500 in chips)", "Seat 2: chiyuki (500 in chips)", "Seat 3: The Borsh (500 in chips)", 
+                                                               "Seat 4: ElenaDenisov (500 in chips)", "Seat 5: V_Andreich (500 in chips)", "Seat 6: PitTheGreek (500 in chips)",
+                                                               "SLIMEE_11: posts the ante 10"], 0);
     },
-    "test getHeroName": function() {
+    "test getHeroName null": function() {
         assertEquals(null, this.reader.getHeroName([]));
+    },
+    "test getHeroName 1 line": function() {
+        assertEquals("HERO", this.reader.getHeroName(["Dealt to HERO [5c 6c]"]));
+    },
+    
+    "test getStartingChip 1 line": function() {
+        assertEquals(1000, this.reader.getStartingChip("HERO", ["Seat 6: HERO (1000 in chips)"], 500));
+    },
+
+    "test setBBSB": function() {
+        assertEquals(10, this.tableData.SB);
+        assertEquals(20, this.tableData.BB);
+        this.reader.setBBSB(this.tableData, "PokerStars Hand #92440992009: Tournament #674374357, $9.07+$0.18 USD Hold'em No Limit - Level I (25/50) - 2013/01/16 22:11:08 JST [2013/01/16 8:11:08 ET]");
+        assertEquals(25, this.tableData.SB);
+        assertEquals(50, this.tableData.BB);
+    },
+
+    "test getButtonPos": function() {
+        assertEquals(1, this.reader.getButtonPos("Table '674374357 1' 6-max Seat #1 is the button"));
+    },
+
+    "test getStartSituation": function() {
+        var result = this.reader.getStartSituation(this.tableData, ["Table '674374357 1' 6-max Seat #1 is the button",
+                                                               "Seat 1: SLIMEE_11 (500 in chips)", "Seat 2: chiyuki (500 in chips)", "Seat 3: The Borsh (500 in chips)", 
+                                                               "Seat 4: ElenaDenisov (500 in chips)", "Seat 5: V_Andreich (500 in chips)", "Seat 6: PitTheGreek (500 in chips)",
+                                                               "SLIMEE_11: posts the ante 10"], 0);
+        assertEquals(6, result);
+        assertEquals([1, 2, 3, 4, 5, 6], this.tableData.seats);
+        assertEquals(["SLIMEE_11", "chiyuki", "The Borsh", "ElenaDenisov", "V_Andreich", "PitTheGreek"], this.tableData.playerNames);
+        assertEquals([500, 500, 500, 500, 500, 500], this.tableData.chips);
+    },
+
+    "test calcAntePayment no Ante": function() {
+        var result = this.reader.calcAntePayment(this.tableData, ["Seat 6: PitTheGreek (500 in chips)", "chiyuki: posts small blind 25"], 0);
+        assertEquals(0, result);
+        assertEquals(0, this.tableData.Ante);
+        assertEquals(0, this.tableData.pot);
+        assertEquals([500, 500, 500, 500, 500, 500], this.tableData.chips);
+    },
+    "test calcAntePayment 10": function() {
+        var result = this.reader.calcAntePayment(this.tableData, ["Seat 6: PitTheGreek (500 in chips)", "SLIMEE_11: posts the ante 10", "chiyuki: posts the ante 10",
+                                                              "The Borsh: posts the ante 10", "ElenaDenisov: posts the ante 10", "V_Andreich: posts the ante 10",
+                                                              "PitTheGreek: posts the ante 10", "chiyuki: posts small blind 25"], 0);
+        assertEquals(6, result);
+        assertEquals(10, this.tableData.Ante);
+        assertEquals(60, this.tableData.pot);
+        assertEquals([490, 490, 490, 490, 490, 490], this.tableData.chips);
+    },
+
+    "test calcBlindPayment SB": function() {
+        var result = this.reader.calcBlindPayment("small", this.tableData, ["PitTheGreek: posts the ante 10", "chiyuki: posts small blind 25", "The Borsh: posts big blind 50"], 0);
+        assertEquals(1, result);
+        assertEquals(25, this.tableData.pot);
+        assertEquals(25, this.tableData.posted[1]);
+        assertEquals([500, 475, 500, 500, 500, 500], this.tableData.chips);
+    },
+    "test calcBlindPayment BB": function() {
+        var result = this.reader.calcBlindPayment("big", this.tableData, ["chiyuki: posts small blind 25", "The Borsh: posts big blind 50", "*** HOLE CARDS ***"], 0);
+        assertEquals(1, result);
+        assertEquals(50, this.tableData.pot);
+        assertEquals(50, this.tableData.posted[2]);
+        assertEquals([500, 500, 450, 500, 500, 500], this.tableData.chips);
+    },
+    "test calcBlindsPayment": function() {
+        var result = this.reader.calcBlindsPayment(this.tableData, ["PitTheGreek: posts the ante 10", "chiyuki: posts small blind 25", 
+                                                                "The Borsh: posts big blind 50", "*** HOLE CARDS ***"], 0);
+        assertEquals(2, result);
+        assertEquals(75, this.tableData.pot);
+        assertEquals(25, this.tableData.posted[1]);
+        assertEquals(50, this.tableData.posted[2]);
+        assertEquals([500, 475, 450, 500, 500, 500], this.tableData.chips);
+    },
+
+    "test calcBetPayment false": function() {
+        var result = this.reader.calcBetPayment(this.tableData, "ElenaDenisov: folds");
+        assertEquals(false, result);
+    },
+    "test calcBetPayment true": function() {
+        var result = this.reader.calcBetPayment(this.tableData, "");
+        assertEquals(true, result);
+    },
+
+    "test calcCallPayment false": function() {
+        var result = this.reader.calcCallPayment(this.tableData, "ElenaDenisov: folds");
+        assertEquals(false, result);
+    },
+    "test calcCallPayment true": function() {
+        var result = this.reader.calcCallPayment(this.tableData, "PitTheGreek: calls 385 and is all-in");
+        assertEquals(true, result);
+        assertEquals(385, this.tableData.pot);
+        assertEquals(385, this.tableData.posted[5]);
+        assertEquals([500, 500, 500, 500, 500, 115], this.tableData.chips);
+    },
+
+    "test calcRaisePayment false": function() {
+        var result = this.reader.calcRaisePayment(this.tableData, "ElenaDenisov: folds");
+        assertEquals(false, result);
+    },
+    "test calcRaisePayment true": function() {
+        var result = this.reader.calcRaisePayment(this.tableData, "chiyuki: raises 440 to 490 and is all-in");
+        assertEquals(true, result);
+        assertEquals(490, this.tableData.pot);
+        assertEquals(490, this.tableData.posted[1]);
+        assertEquals([500, 10, 500, 500, 500, 500], this.tableData.chips);
+    },
+    
+    "test calcUncalledPayment false": function() {
+        var result = this.reader.calcUncalledPayment(this.tableData, "ElenaDenisov: folds");
+        assertEquals(false, result);
+    },
+    "test calcUncalledPayment true": function() {
+        var result = this.reader.calcUncalledPayment(this.tableData, "");
+        assertEquals(true, result);
     }
+
+
 });
